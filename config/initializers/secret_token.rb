8f9480c65232a926807771a0672fbfc268171be2
linjama <matti.linjama@tut.fi>
2013-12-17 21:02:17 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boggle::Application.config.secret_key_base = 'fdbf79d95f5ee647f4d113d572b4b76eda040344de3ceb7bcbb3693ccb353e0cc48b34fbc523c4564c0da82567754bb79572a7421f5f815c2c9ed97ffdaed512'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Boggle::Application.config.session_store :cookie_store, key: '_boggle_session'
