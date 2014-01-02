bd144ee87585d83a783e3ac6e71505f9c6409f6c
linjama <matti.linjama@tut.fi>
2014-01-02 19:00:57 +0200

    Implemented using true Boggle dices instead of random letters

--- app/models/boggle_game.rb
+++ app/models/boggle_game.rb
@@ -3,48 +3,34 @@
 class BoggleGame < ActiveRecord::Base
   
   def generate_letters
-    self.letters = Array.new(16) { 
-      generate_random_letter
+    Random.new_seed
+    dices = self.boggle_dices.shuffle
+    self.letters = Array.new(16) { |ind| 
+      dices[ind].shuffle[0]
     }.join
   end
   
-  def generate_random_letter
-    Random.new_seed
-    letter_position = (10000*rand).floor
-    ind = cumsum(letter_frequencies.values).index { |val| val > letter_position }
-    letter_frequencies.keys[ind]
-  end
-  
-  def cumsum(arr)
-    sum = 0
-    arr.map { |e| sum += e }
-  end
-  
-  def letter_frequencies
-    # Letter frequencies, unit = 1/10000
-    # Source www.cs.tut.fi/~korpela/kielikello/kirjtil.html
-    {
-      'A' => 1192 ,
-      'I' => 1066 ,
-      'T' => 979 ,
-      'N' => 869 ,
-      'E' => 823 ,
-      'S' => 787 ,
-      'L' => 567 ,
-      'K' => 535 ,
-      'O' => 525 ,
-      'U' => 507 ,
-      'Ä' => 460 ,
-      'M' => 331 ,
-      'V' => 253 ,
-      'R' => 232 ,
-      'J' => 191 ,
-      'H' => 183 ,
-      'Y' => 179 ,
-      'P' => 174 ,
-      'D' => 85 ,
-      'Ö' => 49 ,
-      'G' => 13
-    }
+  def boggle_dices
+    # Define Boggle dices
+    # Source http://fi.wikipedia.org/wiki/Boggle
+    [ 
+      ['V', 'L', 'L', 'L', 'E', 'V'],
+      ['O', 'I', 'T', 'T', 'A', 'A'],
+      ['A', 'L', 'N', 'A', 'S', 'T'],
+      ['A', 'N', 'P', 'F', 'S', 'K'],
+      ['A', 'P', 'H', 'S', 'K', 'O'],
+      ['D', 'E', 'S', 'R', 'I', 'L'],
+      ['E', 'I', 'E', 'N', 'U', 'S'],
+      ['H', 'I', 'K', 'N', 'M', 'U'],
+      ['A', 'G', 'A', 'Ä', 'L', 'Ä'],
+      ['C', 'I', 'O', 'T', 'M', 'U'],
+      ['A', 'J', 'T', 'O', 'T', 'O'],
+      ['E', 'I', 'T', 'O', 'S', 'S'],
+      ['E', 'L', 'Y', 'T', 'T', 'R'],
+      ['A', 'K', 'I', 'T', 'M', 'V'],
+      ['A', 'I', 'L', 'K', 'V', 'Y'],
+      ['A', 'L', 'R', 'N', 'N', 'U']
+    ]   
+    
   end
 end
